b3489b2a237c78ac4dc8a8d14df004d37323b689
dlwaddell-cloud <dlwaddell@gmail.com>
2025-10-18 11:39:37 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'bar-darts-cache-v1';
+const urlsToCache = [
+  'BarDarts.html',
+  '501Darts.html',
+  'DartsCricket.html',
+  'NSCricket.html',
+  'DartsHalveIt.html',
+  'DartsGolf.html',
+  'KillerDarts.html',
+  'index.html',
+  'icon-192x192.png',
+  'icon-512x512.png',
+  'apple-touch-icon.png',
+  'favicon.ico'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
